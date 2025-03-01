157a5bce4054f73cd11a585db64e4106d884d460
s1mpampun41k <132660824+s1mpampun41k@users.noreply.github.com>
2025-03-01 19:41:47 +0300

    Initial Commit

---
+++ SwiftBook_HomeWork_2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBook_HomeWork_2.1
+//
+//  Created by Ярослав Шепелевский on 1.03.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
